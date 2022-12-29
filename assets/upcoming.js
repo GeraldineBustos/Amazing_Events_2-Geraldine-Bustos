a6e3b5c93292903e269115df691a7531a5506780
GeraldineBustos <geraldineab.soto@gmail.com>
2022-12-29 00:59:01 -0300

    task 2
    task 2

---
+++ assets/upcoming.js
@@ -0,0 +1,27 @@
+let section = document.getElementById("cards-section2")
+
+function crearCards(lista, cards){
+    let todasLasCards=""
+  for (let recorrido of lista.events){
+    if (recorrido.date <= lista.currentDate){
+        let template=
+        `<div class="card" style="width: 18rem; height: 25rem;">
+        <img src="${recorrido.image}" class="card-img-top " alt="Bootstrap"
+                  style="padding: 5px; ">
+                <div class="card-body">
+                  <h5 class="card-title">${recorrido.name}</h5>
+                  <p class="card-text">${recorrido.description}</p>
+                  <div class="footerCards">
+                    <p>Price: ${recorrido.price}</p>
+                    <a href="./details.html" class="btn btn-primary">Ver Mas</a>
+                </div>
+              </div>
+              </div>`
+              todasLasCards += template 
+  }
+  console.log (cards)
+  cards.innerHTML = todasLasCards
+}
+}
+      crearCards (data, section)
+  
